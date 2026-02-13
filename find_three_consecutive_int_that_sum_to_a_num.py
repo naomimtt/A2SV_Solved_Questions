210c0447ec23f515b32ff3ff3d867d2cc2b17894
naomimtt <naomi.mesfin@a2sv.org>
2026-02-13 15:16:56 +0300

    Create find_three_consecutive_int_that_sum_to_a_num.py

---
+++ find_three_consecutive_int_that_sum_to_a_num.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfThree(self, num: int) -> List[int]:
+        if num % 3 == 0:
+            x = num // 3
+            return [x-1, x, x+1]
+        return []
+
+        
