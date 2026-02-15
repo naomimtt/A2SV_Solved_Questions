84ffe0d851c085a2697eacff615e592ad8781a6d
naomimtt <naomi.mesfin@a2sv.org>
2026-02-15 23:17:30 +0300

    Create Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.py

---
+++ Determine_Whether_Matrix_Can_Be_Obtained_By_Rotation.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        for _ in range(4):
+            if mat == target:
+                return True
+            for i in range(len(mat)):
+                for j in range(i + 1, len(mat)):
+                    mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+            for row in mat:
+                row.reverse()
+        return False
+
+        
