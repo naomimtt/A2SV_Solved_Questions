3c59db51a9457d7396e38955e867d4c9470a8673
naomimtt <naomi.mesfin@a2sv.org>
2026-03-09 23:34:39 +0300

    Create Removing_Stars_From_a_String.py

---
+++ Removing_Stars_From_a_String.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack=[]
+        i=0
+        while i< len(s) :
+            if s[i]== '*':
+               stack.pop()
+            else:
+               stack.append(s[i])
+            i+=1
+        return ''.join(stack)
+       
+           
+            
+           
